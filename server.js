cd3392b30eaca8d600037183393f40a3f31676cb
Nedaeefard <mohammadnedaei99@gmail.com>
2024-07-26 12:28:33 +0330

    optimized folder structures + codes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 var bodyParser = require("body-parser");
 const path = require("path");
+
 const app = express();
 const port = 3000;
 const routes = require("./routes");
